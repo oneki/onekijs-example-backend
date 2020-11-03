b5ce84c45b9bb45438937a3fa7580def7ebe2ed6
Bruno Franki <bruno.franki@gmail.com>
2020-11-03 22:31:15 +0100

    add https

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: process.env.LETSENCRYPT_PRIVATE_KEY,
+      cert: process.env.LETSENCRYPT_CERT,
+    },
+  });
   app.use(cookieParser());
   await app.listen(4000);
 }
